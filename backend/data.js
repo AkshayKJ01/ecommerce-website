e2f5f2c15974f2e893bfa933de19b79781cd4810
AkshayKJ01 <akshayj00001@gmail.com>
2022-04-12 13:11:51 +0530

    CartScreen

--- backend/data.js
+++ backend/data.js
@@ -1,6 +1,7 @@
 const data = {
   products: [
     {
+      _id: '1',
       name: 'Nike Shirt',
       slug: 'nike-shirt',
       category: 'Shirts',
@@ -13,6 +14,7 @@ const data = {
       description: 'High Quality Shirt',
     },
     {
+      _id: '2',
       name: 'Nike Pant',
       slug: 'nike-pant',
       category: 'Pants',
@@ -25,6 +27,7 @@ const data = {
       description: 'High Quality Pant',
     },
     {
+      _id: '3',
       name: 'Adidas Shirt',
       slug: 'adidas-shirt',
       category: 'Shirts',
@@ -37,6 +40,7 @@ const data = {
       description: 'Quality Shirt',
     },
     {
+      _id: '4',
       name: 'Adidas Pant',
       slug: 'adidas-pant',
       category: 'Pants',
